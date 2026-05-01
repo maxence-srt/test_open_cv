93ca2bc95900df65a81c45da1699968a3d49cdb5
maxence-srt <maxence.ossart@esme.fr>
2026-05-01 20:08:27 +0200

    Update new-setup.sh

--- new-setup.sh
+++ new-setup.sh
@@ -1,2 +1,28 @@
 sudo apt update
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
+curl https://pyenv.run | bash
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+exec $SHELL
+
+pyenv install 3.11.9
+
+pyenv shell 3.11.9        # temporarily use Python 3.11 in this terminal
+python -m venv person_detector_env
+source person_detector_env/bin/activate
+
+python --version  
+
+pip install --upgrade pip
+pip install tflite-runtime numpy opencv-python 
+
+wget https://storage.googleapis.com/download.tensorflow.org/models/tflite/coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip
+unzip coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip
+python person_detector.py
+
+
+
+source person_detector_env/bin/activate
